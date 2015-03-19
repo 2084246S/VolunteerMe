14144ac461c38db1b74e529022f72e303dccb2f5
2063602t <0mn!Pr3s3nt>
2015-03-19 13:37:30 +0000

    added default company to opportunities

--- VolunteerMe_project/VolunteerMe/models.py
+++ VolunteerMe_project/VolunteerMe/models.py
@@ -55,7 +55,7 @@ class Organiser(models.Model):
 class Opportunity(models.Model):
     name = models.CharField(max_length=128, unique=True)
     category = models.CharField(max_length=128, default="Other")
-    company = models.ForeignKey(Organiser)
+    company = models.ForeignKey(Organiser, default=None)
     start_date = models.DateField()
     end_date = models.DateField()
     description = models.TextField()
