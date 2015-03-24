bfaaa7cdd5ce9492825e6cc1e5cad779fe2ca81a
2063602t <2063602t@student.gla.ac.uk>
2015-03-24 13:24:37 +0000

    added login required where needed and clean-up: tried fixing a couple bugs and removed the jank line "user = User.objects.get(username=request.user.username)"

--- VolunteerMe_project/VolunteerMe/views.py
+++ VolunteerMe_project/VolunteerMe/views.py
@@ -1,13 +1,12 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
-from VolunteerMe.models import Application, Opportunity, EditUserProfile,Reply
-from VolunteerMe.forms import UserProfileForm, OpportunityForm, UserProfile, EditUserProfileForm,ApplicationForm
-from VolunteerMe.google_address_search import run_query
-from datetime import datetime
+
 from django.contrib.auth.models import User, Group
-from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.decorators import login_required
+
+from VolunteerMe.models import Application, Opportunity, Reply
+
+from VolunteerMe.forms import UserProfileForm, OpportunityForm, UserProfile
+from VolunteerMe.google_address_search import run_query
 
 
 # Homepage
@@ -33,8 +32,6 @@ def index(request):
 
 # search page
 def search(request):
-
-    result_list = []
     final_result_list = []
     result_list = Opportunity.objects.all()
     query = request.GET.get('suggestion', '')
@@ -48,57 +45,51 @@ def search(request):
 
 
 # volunteer opps applied for
+@login_required
 def profile_opps_applied_for(request):
-    u = User.objects.get(username=request.user.username)
+    user = request.user
     context_dict = {}
-    try:
-        up = UserProfile.objects.get(user=request.user)
+    user_profile = UserProfile.objects.get(user=user)
 
-    except:
-        up = None
-
-    applications = Application.objects.filter(volunteer=u)
+    applications = Application.objects.filter(volunteer=user)
     opportunities_list = []
     for application in applications:
         opportunities_list.append(application.opportunity)
 
     context_dict['opportunities_list'] = opportunities_list
 
-    if up.type == 'o':
+    if user_profile.type == 'o':
         organiser = Opportunity.company
-        context_dict['opp'] = Opportunity.objects.filter(company=up)
+        context_dict['opp'] = Opportunity.objects.filter(company=user_profile)
 
     else:
         pass
 
-    context_dict['user'] = u
-    context_dict['userprofile'] = up
+    context_dict['user'] = user
+    context_dict['userprofile'] = user_profile
 
     return render(request, 'Volunteer_Me/profile_opps_applied_for.html', context_dict)
 
 
-#user profile page
+# user profile page
+@login_required
 def profile(request):
     # get user information
     u = request.user
 
     context_dict = {}
+    up = UserProfile.objects.get(user=request.user)
 
-    try:
-        up = UserProfile.objects.get(user=request.user)
-
-    except:
-        up = None
-
-    #if user is an organiser give back their jobs and
+    # if user is an organiser give back their jobs and
     # applications for their jobs
-    if up.type == 'o':
-        opportunities_list = Opportunity.objects.filter(company=up).order_by('-start_date')[:10]
-        #context_dict['app'] = Application.objects.filter(company=up.name)
-    #else give back jobs that are urgent and opportuinities applied for
-    else:
-        opportunities_list = Opportunity.objects.order_by('-start_date')[:10]
-        #context_dict['app'] = Application.objects.filter(name=up.name)
+    if up:
+        if up.type == 'o':
+            opportunities_list = Opportunity.objects.filter(company=up).order_by('-start_date')[:10]
+            # context_dict['app'] = Application.objects.filter(company=up.name)
+        # else give back jobs that are urgent and opportuinities applied for
+        else:
+            opportunities_list = Opportunity.objects.order_by('-start_date')[:10]
+            # context_dict['app'] = Application.objects.filter(name=up.name)
 
     context_dict['opportunities_list'] = opportunities_list
     context_dict['user'] = u
@@ -108,8 +99,8 @@ def profile(request):
 
 
 # place users into one of two groups
-
-def set_group(request, user, user_profile):
+# not a view
+def set_group(user, user_profile):
     if user_profile.type == 'o':
         g = Group.objects.get(name='organiser')
         g.user_set.add(user)
@@ -119,21 +110,21 @@ def set_group(request, user, user_profile):
 
 
 # register
+@login_required
 def register_organiser(request):
     if request.method == 'POST':
         profile_form = UserProfileForm(data=request.POST)
         if profile_form.is_valid():
             if request.user.is_authenticated():
-                profile = profile_form.save(commit=False)
-                user = User.objects.get(username=request.user.username)
-                profile.user = user
-                try:
-                    profile.picture = request.FILES['picture']
-                except:
-                    pass
-                profile.save()
+                proposed_profile = profile_form.save(commit=False)
+                user = request.user
+                proposed_profile.user = user
+                if request.FILES:
+                    if 'picture' in request.FILES:
+                        proposed_profile.picture = request.FILES['picture']
+                proposed_profile.save()
 
-                set_group(request, user, profile)
+                set_group(user, proposed_profile)
 
                 return index(request)
     else:
@@ -141,31 +132,30 @@ def register_organiser(request):
         return render(request, 'Volunteer_Me/organiser/organiser_register.html', {'profile_form': form})
 
 
-#edit profile details
+# edit profile details
+@login_required
 def edit_profile(request):
     if request.method == 'POST':
         profile_form = UserProfileForm(request.POST)
         if profile_form.is_valid():
             if request.user.is_authenticated():
-                profile = profile_form.save(commit=False)
-                user = User.objects.get(username=request.user.username)
-                profile.user = user
-                profile.type = user.type
-                try:
-                    profile.picture = request.FILES['picture']
-                except:
-                    pass
-                UserProfile.objects.get(user=user).delete()
-                profile.save()
+                user = request.user
+                current_profile = UserProfile.objects.get(user=user)
+                current_profile.type = user.type
+                if request.FILES:
+                    if 'picture' in request.FILES:
+                        current_profile.picture = request.FILES['picture']
 
-                set_group(request, user)
+                current_profile.save()
+
+                set_group(user, current_profile)
                 return index(request)
     else:
         form = UserProfileForm()
     return render(request, 'Volunteer_Me/organiser/organiser_register.html', {'profile_form': form})
 
 
-#shows opportunity details
+# shows opportunity details
 def show_opportunity(request, opportunity_id):
     context = dict()
 
@@ -183,25 +173,25 @@ def show_opportunity(request, opportunity_id):
         context['optional'] = opportunity.optional
         context['location'] = opportunity.location
 
-        #Querey address to get lat long
+        # Querey address to get lat long
         location_query_result = run_query(opportunity.location)
 
         if len(location_query_result) > 0:
-            context['latitude'] = location_query_result[0]['lat'];
-            context['longitude'] = location_query_result[0]['lng'];
+            context['latitude'] = location_query_result[0]['lat']
+            context['longitude'] = location_query_result[0]['lng']
 
     if request.method == 'POST':
         profile_form = UserProfileForm(request.POST)
         if profile_form.is_valid():
             if request.user.is_authenticated():
-                profile = profile_form.save(commit=False)
+                proposed_profile = profile_form.save(commit=False)
                 user = User.objects.get(username=request.user.username)
-                profile.user = user
+                proposed_profile.user = user
                 try:
-                    profile.picture = request.FILES['picture']
+                    proposed_profile.picture = request.FILES['picture']
                 except:
                     pass
-                profile.save()
+                proposed_profile.save()
                 g = Group.objects.get(name='organiser')
                 g.user_set.add(user)
 
@@ -217,7 +207,6 @@ def show_opportunity(request, opportunity_id):
 
 @login_required
 def volunteer_replies(request):
-
     # get user information
     u = request.user
 
@@ -232,28 +221,31 @@ def volunteer_replies(request):
     opportunities_list = []
     applications = Application.objects.filter(volunteer=u)
     for application in applications:
-        if(len(Reply.objects.filter(application=application)) > 0):
-            opportunities_list.append({'opp':application.opportunity, 'reply':Reply.objects.filter(application=application)[0]})
+        if len(Reply.objects.filter(application=application)) > 0:
+            opportunities_list.append(
+                {'opp': application.opportunity, 'reply': Reply.objects.filter(application=application)[0]})
 
-    #opportunities_list =Opportunity.objects.order_by('-start_date')[:10]
-    #context_dict['app'] = Application.objects.filter(name=up.name)
+    # opportunities_list =Opportunity.objects.order_by('-start_date')[:10]
+    # context_dict['app'] = Application.objects.filter(name=up.name)
 
     context_dict['opportunities_list'] = opportunities_list
     context_dict['user'] = u
     context_dict['userprofile'] = up
 
-    return render(request,'Volunteer_Me/volunteer/volunteer_replies.html',context_dict)
+    return render(request, 'Volunteer_Me/volunteer/volunteer_replies.html', context_dict)
+
 
 @login_required
 def manage_opportunities(request):
     u = User.objects.get(user=request.user)
     opportunity = Opportunity.objects.filter(company=u)
-    context_dict=[]
-    context_dict['opportunity']=opportunity
-    return render(request,'Volunteer_Me/organiser/opportunities.html',context_dict)
+    context_dict = []
+    context_dict['opportunity'] = opportunity
+    return render(request, 'Volunteer_Me/organiser/opportunities.html', context_dict)
+
 
 @login_required
-#edit specific opportunites
+# edit specific opportunites
 def manage_opportunity(request, opportunity_id, username):
     opportunity = Opportunity.objects.get(id=opportunity_id).filter(username=username)
     if opportunity:
@@ -265,7 +257,7 @@ def manage_opportunity(request, opportunity_id, username):
 
 
 @login_required
-#create an opportunity
+# create an opportunity
 def create_opportunity(request):
     company = request.user
     if request.method == 'POST':
@@ -273,46 +265,51 @@ def create_opportunity(request):
         print("Hello")
         if opp_form.is_valid():
             if request.user.is_authenticated():
-                profile = opp_form.save(commit=False)
-                profile.company = UserProfile.objects.filter(user=company)[0]
-                profile.save()
-                
+                new_opportunity = opp_form.save(commit=False)
+                new_opportunity.company = UserProfile.objects.filter(user=company)[0]
+                new_opportunity.save()
+
                 return redirect('profile')
 
     form = OpportunityForm()
     return render(request, 'Volunteer_Me/organiser/new_opportunity.html', {'opportunity_form': form})
 
 
-#edit current opportunities
+# edit current opportunities
+@login_required
 def edit_opportunity(request):
-    username = User.objects.get(username=request.user.username)
+    # never write:
+    #   username = User.objects.get(username=request.user.username)
+    # instead write:
+    username = request.user
     company = UserProfile.objects.get(name=request.user)
     if request.method == 'POST':
         opp_form = OpportunityForm(request.POST)
         if opp_form.is_valid():
             if request.user.is_authenticated():
-                profile = opp_form.save(commit=False)
-                profile.company = company
-                profile.save()
-                return profile(request)
+                new_opportunity = opp_form.save(commit=False)
+                new_opportunity.company = company
+                new_opportunity.save()
+                return new_opportunity(request)
     else:
         form = OpportunityForm(request.GET)
     return render(request, 'Volunteer_Me/organiser/edit_opportunity.html', {'opportunity_form': form})
 
-#organiser replies from volunteers
+
+# organiser replies from volunteers
 @login_required
 def manage_applications(request):
     context_dict = {}
     u = request.user
 
-    up = UserProfile.objects.filter(user = u)
+    up = UserProfile.objects.filter(user=u)
     opportunities = Opportunity.objects.filter(company=up)
     app_list = []
     app_up = {}
     for opportunity in opportunities:
         applications = Application.objects.filter(opportunity=opportunity)
         for app in applications:
-            app_dict = {'app':app, 'up':UserProfile.objects.filter(user=app.volunteer)[0]}
+            app_dict = {'app': app, 'up': UserProfile.objects.filter(user=app.volunteer)[0]}
             if Reply.objects.filter(application=app):
                 app_dict['reply'] = Reply.objects.filter(application=app)[0]
             app_list.append(app_dict)
@@ -321,8 +318,8 @@ def manage_applications(request):
     context_dict['applications'] = app_list
     return render(request, 'Volunteer_Me/organiser/organiser_replies.html', context_dict)
 
+
 def send_reply(request):
-    
     if request.method == 'POST':
         application_id = request.POST.get('app_id', '')
 
@@ -340,7 +337,8 @@ def send_reply(request):
 
     return redirect('profile')
 
-#mange reply to volunteer
+
+# manage reply to volunteer
 @login_required
 def manage_application(request, application_id):
     application = Opportunity.objects.get(id=application_id)
@@ -351,20 +349,20 @@ def manage_application(request, application_id):
     context = {'application': application}
     return render(request, 'Volunteer_Me/volunteer/volunteer_replies.html', context)
 
-#about page
+
+# about page
 def about(request):
     return render(request, 'Volunteer_Me/about.html')
 
-#jobs list
+
+# jobs list
 def get_job_list(max_results=0, contains=''):
-    job_list = []
-    if contains:
-        job_list = Opportunity.objects.filter(name__contains=contains)
+    if contains != '':
+        job_list = Opportunity.objects.filter(name__icontains=contains)
     else:
         job_list = Opportunity.objects.all()
     if max_results > 0:
-        if len(job_list) > max_results:
-            job_list = job_list[:max_results]
+        job_list = job_list[:max_results]
     return job_list
 
 
@@ -374,36 +372,38 @@ def suggest_job(request):
     contains = ''
     if request.method == 'GET':
         if 'suggestion' in request.GET:
-            contains = request.GET['suggestion']  #get the sting to search for
+            contains = request.GET['suggestion']  # get the sting to search for
 
-    #get 8 placements which contain the string
+    # get 8 placements which contain the string
     cat_list = get_job_list(8, contains)
-    #render in list
+    # render in list
 
     return render(request, 'Volunteer_Me/cats.html', {'cat_list': cat_list})
 
-def application_form(request, opportunity_id):
-    if request.user.is_authenticated():
-        application = Application()
-        application.volunteer = request.user
-        application.opportunity= Opportunity.objects.get(id=opportunity_id)
-        application.save()
-        return redirect('profile')
-    else:
-        return redirect('auth_login')
-    if request.method == 'POST':
-        return redirect('auth-login')
-        application_form = ApplicationForm(request.POST)
-        if application_form.is_valid():
-            application = application_form.save(commit=False)
-            application.opportunity = Opportunity.objects.get(id=opportunity_id)
-            try:
-                profile.picture = request.FILES['picture']
-            except:
-                pass
-            profile.save()
 
-            return index(request)
-    else:
-        form = ApplicationForm(request.GET)
-    return render(request, 'Volunteer_Me/volunteer/applications_form.html', {'profile_form': form})
+@login_required
+def application_form(request, opportunity_id):
+    # if request.user.is_authenticated():
+    application = Application()
+    application.volunteer = request.user
+    application.opportunity = Opportunity.objects.get(id=opportunity_id)
+    application.save()
+    return redirect('profile')
+    # else:
+    # return redirect('auth_login')
+    # if request.method == 'POST':
+    # return redirect('auth-login')
+    # application_form = ApplicationForm(request.POST)
+    # if application_form.is_valid():
+    # application = application_form.save(commit=False)
+    # application.opportunity = Opportunity.objects.get(id=opportunity_id)
+    # try:
+    # profile.picture = request.FILES['picture']
+    # except:
+    # pass
+    # profile.save()
+    #
+    # return index(request)
+    # else:
+    # form = ApplicationForm(request.GET)
+    # return render(request, 'Volunteer_Me/volunteer/applications_form.html', {'profile_form': form})
